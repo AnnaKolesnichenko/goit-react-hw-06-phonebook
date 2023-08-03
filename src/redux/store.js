d6d205b1fe326c398d18251962cb1e70eebc5845
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-08-03 20:54:32 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { contactDetailsReducer } from "./contactsReducer";
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
